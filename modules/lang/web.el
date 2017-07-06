b85568ca9355a0506df374147689f690492ace8a
Sequoia Snow <the1codemaster@gmail.com>
2017-07-06 00:02:45 -0700

    All the icons made all the difference

--- modules/lang/web.el
+++ modules/lang/web.el
@@ -1,16 +1,34 @@
 (use-package web-mode
   :config
-  (progn
-    (defun snow-add-web-auto (extension)
-      (add-to-list 'auto-mode-alist  (cons
-                                      (concat "\\" extension "$") web-mode)))
-    (mapcar 'snow-add-web-auto '(".js"
-                                 ".jsx"
-                                 ".html"
-                                 ".ms"
-                                 ".erb"
-                                 ".mustache"
-                                 ".scss"
-                                 ".tsx")))
+  (add-to-list 'auto-mode-alist '("\\.js\\'"       . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.jsx\\'"      . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html\\'"     . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.ms\\'"       . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'"      . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.scss\\'"     . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'"      . web-mode))
   (setq web-mode-content-type-alist
         '(("jsx" . "\\.js[x]?$"))))
+
+(use-package tide
+  :after web-mode
+  :config
+  (defun setup-tide-mode ()
+    (interactive)
+    (tide-setup)
+    (flycheck-mode +1)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode +1)
+    (tide-hl-identifier-mode +1)
+    ;; company is an optional dependency. You have to
+    ;; install it separately via package-install
+    ;; `M-x package-install [ret] company`
+    (company-mode +1))
+  (setq company-tooltip-align-annotations t)
+  (add-hook 'typescript-mode-hook #'setup-tide-mode)
+  (add-hook 'web-mode-hook
+            (lambda ()
+              (when (strin-equal "tsx" (file-name-extension buffer-file-name))
+                (setup-tide)))))
+
