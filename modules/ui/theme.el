614a8fa18b0c16a2cd9a0323766dfdc9a29c2af2
the1codemaster <the1codemaster@gmail.com>
2018-07-03 16:43:44 -0700

    removed soalire mode for the time being because of complications

--- modules/ui/theme.el
+++ modules/ui/theme.el
@@ -6,6 +6,7 @@
   (load-theme 'doom-dracula t))
 
 (use-package solaire-mode
+  :disabled ;; Until margin highlighting is ok again
   :config
   ;; brighten buffers (that represent real files)
   (add-hook 'after-change-major-mode-hook #'turn-on-solaire-mode)
@@ -16,9 +17,6 @@
   (defun snow|recolor-solaire-fringes ()
     (unless (solaire-mode--real-buffer-fn (current-buffer))
       (set-window-fringes (get-buffer-window (current-buffer)) 0 0 nil)))
-
-  (add-hook 'after-change-major-mode-hook 'snow|recolor-solaire-fringes)
-  (add-hook 'ediff-prepare-buffer-hook 'snow|recolor-solaire-fringes)
 
   ;; if the bright and dark background colors are the wrong way around, use this
   ;; to switch the backgrounds of the `default` and `solaire-default-face` faces.
